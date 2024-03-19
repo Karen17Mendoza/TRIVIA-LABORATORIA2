7d586518d60f1038bc20b7fd07f6225d02759f34
Glitch (glitch-hello-website) <none>
2024-03-19 19:23:16 +0000

    👚🌲 Checkpoint ./index.html:66042606/395 ./script.js:66042606/240

---
+++ script.js
@@ -0,0 +1,6 @@
+function iniciarJuego() {
+    // Ocultar la pantalla de bienvenida
+    document.getElementById("bienvenida").style.display = "none";
+    // Mostrar la pantalla de preguntas
+    document.getElementById("preguntas").style.display = "block";
+}
